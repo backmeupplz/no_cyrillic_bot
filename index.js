c175f8c422bb87e0d305e0b04217c6f3614d1506
Nikita Kolmogorov <backmeupplz@gmail.com>
2019-04-01 14:48:12 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,15 @@ require('dotenv').config({ path: `${__dirname}/.env` })
 const bot = new Telegraf(process.env.TOKEN)
 
 bot.use(async (ctx, next) => {
-  if (ctx.message) {
-    const message = ctx.editedMessage || ctx.message
-    if (message) {
-      const cyrillicPattern = /[\u0400-\u04FF]/
-      if (cyrillicPattern.test(message.text)) {
-        try {
-          await ctx.deleteMessage()
-        } catch (err) {
-          // Do nothing
-        }
-      }
+  try {
+    const message = ctx.message
+    const editedMessage = ctx.editedMessage
+    const cyrillicPattern = /[\u0400-\u04FF]/
+    if ((message && cyrillicPattern.test(message.text)) || (editedMessage && cyrillicPattern.test(editedMessage.text))) {
+        await ctx.deleteMessage()
     }
+  } catch (err) {
+    // Do nothing
   }
   next()
 })
